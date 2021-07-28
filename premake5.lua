4b0afe1a1c0824bd3bfb33e895b4047705571f9a
david-bu <79463391+david-bu@users.noreply.github.com>
2021-07-28 16:08:36 +0200

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "assimp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"code/**.cpp",
+    "code/**.h",
+		"code/**.inl",
+		"include/**.h,
+		"include/**.hpp",
+		"include/**.inl"
+	}
+
+	filter "system:Windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "system:Linux"
+		pic "on"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
